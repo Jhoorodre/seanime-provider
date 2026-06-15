94950c539391ead4cdff0ad920988d542e7029f4
Jhoorodre <jhoorodr@example.com>
2026-06-15 05:28:40 -0300

    fix(goyabu): set correct blogger referer for video playback

--- src/goyabu/goyabu.ts
+++ src/goyabu/goyabu.ts
@@ -97,7 +97,10 @@ class Provider {
         
         const result: EpisodeServer = {
             server: "Blogger",
-            headers: this.headers,
+            headers: {
+                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36",
+                "Referer": "https://www.blogger.com/"
+            },
             videoSources: []
         }
 
